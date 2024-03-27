25dcdb27d9f1bea337f3eb41208cb6fd551fb900
Deepanshi <deepanshibhardwaj99@gmail.com>
2024-03-27 15:15:16 +0530

    ac http

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [res, setRes] = useState(null);
 
-export default App;
+  const fetchData = async (numberid) => {
+    try {
+      const res = await axios.get(`http://localhost:9876/numbers/${numberid}`);
+      setRes(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
